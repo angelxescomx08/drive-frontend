e4f58ae4cafcc7d77dbd6d1f20098ce2efb925da
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-06-28 15:04:07 -0600

    feat: agregar lógica para establecer el usuario en el estado de autenticación al cargar la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { QueryClientProvider } from '@tanstack/react-query';
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router';
 import { Toaster } from './components/ui/sonner';
 import { queryClient } from './lib/queryClient';
+import { useAuthStore } from './modules/auth/hooks/useAuthStore';
 import { routes } from './routes/routes';
 
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      useAuthStore.getState().setUser(null);
+    }
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={routes} />
