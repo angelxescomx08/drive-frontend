901a0b9de0d9e084e3af0a5d41f69f696834b0be
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-06-30 18:21:45 -0600

    feat: ajustar el tamaño del Dropzone para mejorar la experiencia de usuario al agregar un padding adicional

--- src/components/ui/customs/dropzone.tsx
+++ src/components/ui/customs/dropzone.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function Dropzone({ children }: Props) {
   return (
-    <div className='w-full h-[calc(100vh-5rem)] border rounded-md shadow-lg flex gap-4'>
+    <div className='w-full h-[calc(100vh-8rem)] border rounded-md shadow-lg flex gap-4 p-4'>
       {children}
     </div>
   );
