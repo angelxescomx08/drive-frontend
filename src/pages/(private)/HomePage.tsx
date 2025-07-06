db52427736a183a1819f0db746b1290e2253c9bd
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-07-05 19:34:56 -0600

    feat: refactorizar la funcionalidad de arrastre y suelta en HomePage para utilizar una función manejadora y mejorar la gestión de archivos y carpetas

--- src/pages/(private)/HomePage.tsx
+++ src/pages/(private)/HomePage.tsx
@@ -22,44 +22,42 @@ export const HomePage = () => {
   const { createFileMutation } = useCreateFile();
   const { user } = useAuthStore();
 
+  const handleDrop = async (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    if (event.dataTransfer && event.dataTransfer.items) {
+      for (let i = 0; i < event.dataTransfer.items.length; i++) {
+        const item = event.dataTransfer.items[i];
+        if (item.webkitGetAsEntry) {
+          const entry = item.webkitGetAsEntry();
+          if (entry?.isDirectory) {
+            createFolderMutation.mutate({
+              folder_name: entry?.name ?? '',
+              id_parent: currentFolder,
+              id_user: user?.id_user ?? '',
+            });
+          }
+          if (entry?.isFile) {
+            const file = await new Promise<File>(resolve => {
+              (entry as FileSystemFileEntry).file(file => resolve(file));
+            });
+            createFileMutation.mutate({
+              file_name: entry?.name ?? '',
+              id_folder: currentFolder,
+              file,
+            });
+          }
+        }
+      }
+    }
+  };
+
   return (
     <>
       <Header />
       <main className='container mx-auto'>
         <ContextMenuComponent>
           <div className='flex gap-4 mt-4'>
-            <Dropzone
-              onDrop={async event => {
-                event.preventDefault();
-                if (event.dataTransfer && event.dataTransfer.items) {
-                  for (let i = 0; i < event.dataTransfer.items.length; i++) {
-                    const item = event.dataTransfer.items[i];
-                    if (item.webkitGetAsEntry) {
-                      const entry = item.webkitGetAsEntry();
-                      if (entry?.isDirectory) {
-                        createFolderMutation.mutate({
-                          folder_name: entry?.name ?? '',
-                          id_parent: null,
-                          id_user: user?.id_user ?? '',
-                        });
-                      }
-                      if (entry?.isFile) {
-                        const file = await new Promise<File>(resolve => {
-                          (entry as FileSystemFileEntry).file(file =>
-                            resolve(file)
-                          );
-                        });
-                        createFileMutation.mutate({
-                          file_name: entry?.name ?? '',
-                          id_folder: null,
-                          file,
-                        });
-                      }
-                    }
-                  }
-                }
-              }}
-            >
+            <Dropzone onDrop={handleDrop}>
               {folderContent.data.folders.map(folder => (
                 <FolderComponent
                   key={folder.id_folder}
